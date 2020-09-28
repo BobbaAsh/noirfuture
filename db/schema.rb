2562410fb4d0eac823b7295e8e6a840572a3aa83
Daniel Kaddous <danielkaddous.ys@gmail.com>
2020-09-28 16:23:01 +0200

    Photo Couverture et Photo

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_09_23_122902) do
+ActiveRecord::Schema.define(version: 2020_09_28_114303) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -42,6 +42,8 @@ ActiveRecord::Schema.define(version: 2020_09_23_122902) do
     t.datetime "updated_at", precision: 6, null: false
     t.string "name"
     t.string "category"
+    t.string "photo"
+    t.string "photos"
   end
 
   create_table "users", force: :cascade do |t|
