2e09ca599a1a49a3093b52bc959768cf3435820f
Daniel Kaddous <danielkaddous.ys@gmail.com>
2020-09-30 16:15:22 +0200

    Commit

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_09_30_123258) do
+ActiveRecord::Schema.define(version: 2020_09_29_134552) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -56,7 +56,6 @@ ActiveRecord::Schema.define(version: 2020_09_30_123258) do
     t.string "photo"
     t.string "photos"
     t.string "slug"
-    t.string "link"
     t.index ["slug"], name: "index_posts_on_slug", unique: true
   end
 
