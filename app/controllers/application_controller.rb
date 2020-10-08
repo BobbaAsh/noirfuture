45f10230b5b04e22b1e5ee00c00f157702193a6f
Daniel Kaddous <danielkaddous.ys@gmail.com>
2020-10-08 13:30:53 +0200

    fix

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,16 +1,9 @@
 class ApplicationController < ActionController::Base
   before_action :authenticate_user!
-  before_filter :add_www_subdomain
 
 
   def default_url_options
     { host: ENV["DOMAIN"] || "localhost:3000" }
   end
 
-    private
-  def add_www_subdomain
-    unless /^www/.match(request.host)
-      redirect_to("#{request.protocol}x.com#{request.request_uri}",
-                  :status => 301)
-    end
 end
